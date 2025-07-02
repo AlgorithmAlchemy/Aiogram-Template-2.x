05be03cbafc6ddcb7073dc68a56e4fcfffdb5782
AlgorithmAlchemy <darkydark@cyberfear.com>
2025-07-03 02:44:43 +0300

    Initial commit

--- handlers/users/callback/all_callback.py
+++ handlers/users/callback/all_callback.py
@@ -9,10 +9,6 @@ from aiogram.types import Update
 
 from loader import dp, bot
 import sqlite3
-from data.config import demo_menu_message, one_step_confirm, data, \
-    demo_day_counter, full_access_message, generate_demo_message, CHAT
-from keyboards.inline import home_menu, terms_of_use_menu, main_menu_limited_access, \
-    region_selection_menu, support_menu, pay_menu, before_pay_menu
 from models.sqlite3_creator import db, connect
 
 from datetime import datetime
--- keyboards/inline/__init__.py
+++ keyboards/inline/__init__.py
@@ -4,8 +4,3 @@ from aiogram.types import ReplyKeyboardRemove, \
     InlineKeyboardMarkup, InlineKeyboardButton
 
 
-def terms_of_use_menu():
-    terms_of_use_accept = InlineKeyboardButton('Хочу подключить 👌', callback_data='accept_terms_of_use')
-    menu = InlineKeyboardMarkup(row_width=1, resize_keyboard=True)
-    menu.add(terms_of_use_accept)
-    return menu
